3c31e5a1fb77f655885c4f087544db59244f6534
Peter Valdez <ptr.vldz@gmail.com>
2024-09-15 19:23:06 -0400

    Update tesh.sh

--- tesh.sh
+++ tesh.sh
@@ -2,4 +2,4 @@
 # This script is used to test the address validation endpoint
 curl -X POST http://localhost:8080/api/address/validate \
      -H "Content-Type: application/json" \
-     -d '{ "address": "3050 21st st", "city": "queens", "state": "NY" }' | jq -r .data | jq
+     -d '{ "address": "3341 29th st", "city": "queens", "state": "NY" }' | jq -r .data | jq
